7ddf9e9618f74a4a14df15a98a7fc95e26761be0
Ronan Tunney <rjtunney@edu.uwaterloo.ca>
2025-04-17 22:20:50 -0400

    Removing Webassembly work :(

--- src/wasmLoader.ts
+++
@@ -1,9 +0,0 @@
-//@ts-ignore
-import initWasm from "./wasm/sph.js"; // Import the generated JS file
-
-async function loadWasm() {
-  const wasmModule = await initWasm(); // Load WASM
-  return wasmModule;
-}
-
-export default loadWasm;
--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,4 @@ export default defineConfig({
       include: "**/*.wgsl",
     }),
   ],
-  assetsInclude: ["**/*.wasm"],
 });
